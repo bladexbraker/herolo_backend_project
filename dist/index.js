33a02d210b2a989fd5e0514f2980f5026375846e
unknown <bladexbreaker@gmail.com>
2020-09-27 23:26:57 +0300

    changed to fix heroku

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,8 @@ var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var routes_1 = require("./routes");
+require("tsconfig-paths/register");
+require("ts-node/register");
 dotenv_1.default.config();
 var app = express_1.default();
 app.use(body_parser_1.default.json());
